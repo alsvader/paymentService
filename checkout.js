b94392b5d9042ca26edaf6e191ca03180a4923d1
aaronUnosquare <88786786+aaronUnosquare@users.noreply.github.com>
2022-06-02 00:51:56 -0500

    Add payment service page

---
+++ checkout.js
@@ -0,0 +1,46 @@
+function initPayPalButton() {
+	paypal
+		.Buttons({
+			style: {
+				shape: 'rect',
+				color: 'gold',
+				layout: 'vertical',
+				label: 'pay',
+			},
+
+			createOrder: function (data, actions) {
+				return actions.order.create({
+					purchase_units: [
+						{
+							description: 'AppCompañarte',
+							amount: { currency_code: 'USD', value: 150 },
+						},
+					],
+				});
+			},
+
+			onApprove: function (data, actions) {
+				return actions.order.capture().then(function (orderData) {
+					// Full available details
+					console.log(
+						'Capture result',
+						orderData,
+						JSON.stringify(orderData, null, 2),
+					);
+
+					// Show a success message within this page, e.g.
+					const element = document.getElementById('paypal-button-container');
+					element.innerHTML = '';
+					element.innerHTML = '<h3>¡Gracias por su pago!</h3>';
+
+					// Or go to another URL:  actions.redirect('thank_you.html');
+				});
+			},
+
+			onError: function (err) {
+				console.log(err);
+			},
+		})
+		.render('#paypal-button-container');
+}
+initPayPalButton();
